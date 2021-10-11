8f73698d7c38fc364dca42cb8fd0f1b9153fd46d
JoshDaos <joshuadaos1998@gmail.com>
2021-10-11 13:05:14 -0700

    working on displaying data

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,16 @@
 import Axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [film, setFilm] = useState('')
 
   const getAnime = () => {
     Axios.get('https://ghibliapi.herokuapp.com/films').then(
       (response) => {
         console.log(response);
+        setFilm(response.data.setup + '...' + response.data.id)
     });
   };
 
@@ -15,6 +19,7 @@ function App() {
     <div className="App">
     Fetching API calls
     <button onClick={getAnime}>Get Film</button>
+    {film}
     </div>
   );
 }
