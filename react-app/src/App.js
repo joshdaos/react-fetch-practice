7902b0d91e1e0dce6268179a4e9bb7f7b8243a5f
JoshDaos <joshuadaos1998@gmail.com>
2021-10-11 12:53:15 -0700

    using axios to fetch api

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,20 @@
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const getAnime = () => {
+    Axios.get('https://ghibliapi.herokuapp.com/films').then(
+      (response) => {
+        console.log(response);
+    });
+  };
+
+
   return (
     <div className="App">
-   Fetching API calls
+    Fetching API calls
+    <button onClick={getAnime}>Get Film</button>
     </div>
   );
 }
