3d439cc5246d2fae00fa48aa8047154ad90450d5
JoshDaos <joshuadaos1998@gmail.com>
2021-10-11 16:28:07 -0700

    connected to a film

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,20 @@
 import Axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [film, setFilm] = useState('')
+  const [film, setFilm] = useState([])
 
   const getAnime = () => {
-    Axios.get('https://ghibliapi.herokuapp.com/films').then(
+    Axios.get('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49').then(
       (response) => {
         console.log(response);
-        setFilm(response.data.setup + '...' + response.data.id)
+        setFilm(response.data.title + '...' + response.data.description)
     });
   };
 
+  useEffect(() => getAnime, [])
 
   return (
     <div className="App">
